e800b50ce7e798292c1e02ddd703abac72981cf7
James Galvin <jgalvin@starcircle.com>
2026-06-11 23:45:00 +0100

    Wire live Web3Forms access key (form now delivers to info@vsource.net)

--- src/data/site.js
+++ src/data/site.js
@@ -63,7 +63,7 @@ export const different = {
 // redirected to /thanks. Until a real key is set, the forms will not deliver.
 export const forms = {
   endpoint: 'https://api.web3forms.com/submit',
-  accessKey: 'REPLACE_WITH_WEB3FORMS_KEY', // generate at web3forms.com using info@vsource.net
+  accessKey: 'f22786e7-1f38-41e9-8133-c2daa1c34361', // Web3Forms, delivers to info@vsource.net
   redirect: 'https://vsource.net/thanks',
 };
 
